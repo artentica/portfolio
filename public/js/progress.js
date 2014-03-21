bd75b5e76b8b2c295ccb8a1cd9099d25e150bf63
artentica <riouallonvincent@gmail.com>
2014-03-21 17:12:04 +0100

    skill button

--- public/js/progress.js
+++ public/js/progress.js
@@ -15,17 +15,29 @@ var data = {
   	{ value : 85, color : "hsl(120, 33%, 50%)" },
   	{ value : 15, color : "hsl(120, 10%, 90%)" }
   ],
-  javascript: [
-  	{ value : 60, color : "hsl(120, 33%, 50%)" },
-  	{ value : 40, color : "hsl(120, 10%, 90%)" }
-  ],
   php: [
   	{ value : 35, color : "hsl(120, 33%, 50%)" },
   	{ value : 65, color : "hsl(120, 10%, 90%)" }
   ],
-  mysql: [
-  	{ value : 60, color : "hsl(120, 33%, 50%)" },
-  	{ value : 40, color : "hsl(120, 10%, 90%)" }
+  javascript: [
+    { value : 60, color : "hsl(120, 33%, 50%)" },
+    { value : 40, color : "hsl(120, 10%, 90%)" }
+  ],
+  jquery: [
+    { value : 40, color : "hsl(120, 33%, 50%)" },
+    { value : 60, color : "hsl(120, 10%, 90%)" }
+  ],
+  angularjs: [
+    { value : 35, color : "hsl(120, 33%, 50%)" },
+    { value : 65, color : "hsl(120, 10%, 90%)" }
+  ],
+  nodejs: [
+    { value : 20, color : "hsl(120, 33%, 50%)" },
+    { value : 80, color : "hsl(120, 10%, 90%)" }
+  ],
+   apache: [
+    { value : 85, color : "hsl(120, 33%, 50%)" },
+    { value : 15, color : "hsl(120, 10%, 90%)" }
   ],
   java: [
   	{ value : 45, color : "hsl(120, 33%, 50%)" },
@@ -43,17 +55,29 @@ var data = {
     { value : 20, color : "hsl(120, 33%, 50%)" },
     { value : 80, color : "hsl(120, 10%, 90%)" }
   ],
-  apache: [
-    { value : 85, color : "hsl(120, 33%, 50%)" },
-    { value : 15, color : "hsl(120, 10%, 90%)" }
-  ],
-  ccna1: [
+ ccna1: [
     { value : 79, color : "hsl(120, 33%, 50%)" },
     { value : 21, color : "hsl(120, 10%, 90%)" }
   ],
   ccna2: [
     { value : 10, color : "hsl(120, 33%, 50%)" },
     { value : 90, color : "hsl(120, 10%, 90%)" }
+  ],
+  linux: [
+    { value : 50, color : "hsl(120, 33%, 50%)" },
+    { value : 50, color : "hsl(120, 10%, 90%)" }
+  ],
+  windows: [
+    { value : 90, color : "hsl(120, 33%, 50%)" },
+    { value : 10, color : "hsl(120, 10%, 90%)" }
+  ],
+  git: [
+    { value : 90, color : "hsl(120, 33%, 50%)" },
+    { value : 10, color : "hsl(120, 10%, 90%)" }
+  ],
+  mysql: [
+    { value : 60, color : "hsl(120, 33%, 50%)" },
+    { value : 40, color : "hsl(120, 10%, 90%)" }
   ],
   photoshop: [
     { value : 30, color : "hsl(120, 33%, 50%)" },
@@ -70,31 +94,7 @@ var data = {
   c4d: [
     { value : 85, color : "hsl(120, 33%, 50%)" },
     { value : 15, color : "hsl(120, 10%, 90%)" }
-  ],
-  linux: [
-    { value : 50, color : "hsl(120, 33%, 50%)" },
-    { value : 50, color : "hsl(120, 10%, 90%)" }
-  ],
-  windows: [
-    { value : 90, color : "hsl(120, 33%, 50%)" },
-    { value : 10, color : "hsl(120, 10%, 90%)" }
-  ],
-  angularjs: [
-    { value : 35, color : "hsl(120, 33%, 50%)" },
-    { value : 65, color : "hsl(120, 10%, 90%)" }
-  ],
-  nodejs: [
-    { value : 20, color : "hsl(120, 33%, 50%)" },
-    { value : 80, color : "hsl(120, 10%, 90%)" }
-  ],
-  git: [
-    { value : 90, color : "hsl(120, 33%, 50%)" },
-    { value : 10, color : "hsl(120, 10%, 90%)" }
-  ],
-  jquery: [
-    { value : 40, color : "hsl(120, 33%, 50%)" },
-    { value : 60, color : "hsl(120, 10%, 90%)" }
-  ]
+  ]  
 };
 
 var offset = 0;
